06e0b674a3c79cebf2557c3423c27f5626d8cbbc
notjesseolsson <notjesseolsson@gmail.com>
2014-12-19 16:26:52 -0500

    added everything

---
+++ index.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var xhr = new XMLHttpRequest()
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      var list = JSON.parse(xhr.responseText)
+      for (var i = list.length - 1; i > -1; i--) {
+        var elem = document.createElement('img')
+        elem.setAttribute('src', 'pictures/' + list[i])
+        document.body.appendChild(elem)
+      }
+    }
+  }
+  xhr.open('GET', 'list.json')
+  xhr.send()
+})
