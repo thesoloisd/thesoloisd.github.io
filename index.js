b826428e66e130ed3b685394a2b38a9174c373b6
notjesseolsson <notjesseolsson@gmail.com>
2014-12-19 18:39:34 -0500

    remove click on omg

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
+
   var xhr = new XMLHttpRequest()
   xhr.onreadystatechange = function() {
     if (xhr.readyState == 4 && xhr.status == 200) {
@@ -8,17 +9,9 @@ document.addEventListener('DOMContentLoaded', function() {
         elem.setAttribute('src', 'pictures/' + list[i])
         document.body.appendChild(elem)
       }
-
-
-      var pics = document.querySelectorAll('img')
-      for (var i = 0; i < pics.length; i++) {
-        pics[i].onclick = function(e) {
-          location = e.target.src
-        }
-      }
-
     }
   }
   xhr.open('GET', 'list.json')
   xhr.send()
+
 })
